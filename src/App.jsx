bdc02df957d2d34ba0ab44d1ed6ab74cddd19932
Muller Esposito <mulleresposito@hotmail.com>
2021-03-27 20:53:43 -0300

    feat(Game): creates route to Game page;

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 import './App.css';
-import logo from './trivia.png';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
         <Switch>
+          <Route path="/game" component={ Game } />
           <Route path="/" component={ Login } />
         </Switch>
       </header>
---
+++ src/pages/Game/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="container-game">
+        Game Page
+      </div>
+    );
+  }
+}
+
+export default Game;
