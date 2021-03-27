13fec30ec8a6d12ce93965fc97cf2da57757f0e7
Muller Esposito <mulleresposito@hotmail.com>
2021-03-26 23:43:59 -0300

    feat(Redux,Router): creates redux and router environment;

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router';
+
+import Login from './pages/Login';
+
 import './App.css';
+import logo from './trivia.png';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
+        <Switch>
+          <Route path="/" component={ Login } />
+        </Switch>
       </header>
     </div>
   );
