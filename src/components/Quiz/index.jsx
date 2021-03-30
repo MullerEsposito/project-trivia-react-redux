d510bfa152c84292872dfecd88063df048dcd209
Muller Esposito <mulleresposito@hotmail.com>
2021-03-30 16:08:45 -0300

    feat(Quiz): creates functionality to verify the right answer and stop the timer;

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import quizType from '../../types';
 
 import './style.css';
 
 class Quiz extends Component {
+  constructor() {
+    super();
+
+    this.handleClickButton = this.handleClickButton.bind(this);
+  }
+
+  handleClickButton({ target: { innerText: answer } }) {
+    const { quiz: { correct_answer: right }, timer } = this.props;
+    const buttons = document.querySelectorAll('button');
+
+    clearInterval(timer);
+    this.verifiesAnswer(answer, right);
+
+    buttons.forEach((button) => {
+      button.classList.add(button.innerText === right ? 'right' : 'wrong');
+    });
+  }
+
+  verifiesAnswer(answer, correctAnswer) {
+    const { quiz: { difficulty }, score } = this.props;
+    const time = document.getElementById('timer').innerText;
+    const { player } = JSON.parse(localStorage.getItem('state'));
+
+    const levels = { hard: 3, medium: 2, easy: 1 };
+    const goal = 10;
+
+    if (answer === correctAnswer) {
+      const newScore = score + goal + (time * levels[difficulty]);
+      player.score = newScore;
+
+      localStorage.setItem('state', JSON.stringify({ player }));
+    }
+  }
+
   renderOptions() {
     const { quiz: { incorrect_answers: wrongs, correct_answer: right } } = this.props;
     const fiftyPercent = 0.5;
 
     const options = wrongs.map((wrong, idx) => (
-      <button data-testid={ `wrong-answer-${idx}` } key={ idx } type="button">
+      <button
+        onClick={ this.handleClickButton }
+        data-testid={ `wrong-answer-${idx}` }
+        key={ idx }
+        type="button"
+      >
         {wrong}
       </button>
     ));
     options.push(
-      <button key={ options.length } data-testid="correct-answer" type="button">
+      <button
+        onClick={ this.handleClickButton }
+        data-testid="correct-answer"
+        key={ options.length }
+        type="button"
+      >
         {right}
       </button>,
     );
@@ -25,7 +71,6 @@ class Quiz extends Component {
 
   render() {
     const { quiz: { category, question } } = this.props;
-
     return (
       <div className="container-quiz">
         <p data-testid="question-category">{`Categoria ${category}`}</p>
@@ -38,6 +83,8 @@ class Quiz extends Component {
 
 Quiz.propTypes = {
   quiz: quizType.isRequired,
+  timer: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  score: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
 };
 
 export default Quiz;
