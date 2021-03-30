55d10aafbe2b58e045417c4ca17c447d7783612c
Muller Esposito <mulleresposito@hotmail.com>
2021-03-30 16:15:54 -0300

    feat(Mock): creates mock file to simulate api trivia response;

---
+++ src/mocks.js
@@ -0,0 +1,14 @@
+const quiz = {
+  category: 'Entertainment: Video Games',
+  type: 'multiple',
+  difficulty: 'easy',
+  question: 'What is the first weapon you acquire in Half-Life?',
+  correct_answer: 'A crowbar',
+  incorrect_answers: [
+    'A pistol',
+    'The H.E.V suit',
+    'Your fists',
+  ],
+};
+
+export default quiz;
